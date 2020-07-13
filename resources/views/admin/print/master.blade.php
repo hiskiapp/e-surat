05b236eddb352e9aa284610fd852a1e47d9edd4e
Hiskia Anggi <hiskianggi@gmail.com>
2020-07-14 00:20:00 +0700

    Improve

--- resources/views/admin/print/master.blade.php
+++ resources/views/admin/print/master.blade.php
@@ -57,7 +57,7 @@
 			<div class="row">
 				<div class="col-12">
 					<h3 class="text-center">{{ $submission->letter->name }}</h3>
-					<p class="text-center h5">{{ $submission->letter->name }}</p>
+					<p class="text-center h5">{{ $submission->number ?? '-' }}</p>
 				</div>
 			</div>
 			<div class="row">
